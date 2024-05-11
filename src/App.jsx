bd79b0d8f34912de5b3859e6bac09b55f403d779
sajib37 <sajibhasan9811@gmail.com>
2024-05-11 15:03:03 +0600

    project set up and folder structure set up completed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>My name is khan</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/About/About.jsx
@@ -0,0 +1,10 @@
+import profile2 from '../../assets/images/profile2.png'
+import "../../App.css"
+const About = () => {
+    return (
+        <section className="max-w-screen-xl text-white mx-auto min-h-screen flex gap-12 items-center justify-center flex-col py-8 pb-12">
+            
+            
+        </section>
+    );
+};
