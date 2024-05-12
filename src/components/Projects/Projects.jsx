9b8ee9735a5fdc727b9c0fd7ff57549c775a0570
sajib37 <sajibhasan9811@gmail.com>
2024-05-12 13:17:12 +0600

    project section completed

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,116 @@
-import React from 'react';
+import React from "react";
+import bookStore from "./../../assets/projects/bookStore.png";
+import campusCommune from "../../assets/projects/campus-commune.png";
+import counseling from "../../assets/projects/counceling.jpg";
+import counter from "../../assets/projects/counter.png";
 
 const Projects = () => {
     return (
-        <div>
-            <h1 className='text-5xl text-white'>my projects section</h1>
-        </div>
+        <section className="max-w-screen-xl pb-10 mx-auto flex flex-col justify-center items-center min-h-screen gap-8 md:gap-12">
+            <h1 className="font-bold mb-2 text-center text-transparent lg:text-6xl text-3xl md:text-4xl bg-clip-text bg-gradient-to-r from-purple-600 to-orange-500">
+                My Projects
+            </h1>
+
+            <section className="text-white px-2 md:px-10 lg:px-0 grid lg:grid-cols-2 gap-6  lg:gap-8">
+                {/* Campus Commune */}
+                <div
+                    className="space-y-2  rounded-xl"
+                    style={{ boxShadow: "0px 0px 10px 0px #9b59b6" }}
+                >
+                    <img
+                        className="rounded-t-xl w-full max-h-80"
+                        src={campusCommune}
+                        alt=""
+                    />
+                    <div className="p-2">
+                        <h1 className="text-lg text-purple-600 font-bold">
+                            Campus commune
+                        </h1>
+                        <p className="text-justify">
+                            “Campus Commune” is a reliable and student friendly
+                            system.That will create strong bonding and
+                            brotherhood among the students of a campus as well
+                            as remove all the hustle in case of accommodation
+                            and campus marketplace gap.
+                        </p>
+                    </div>
+                </div>
+
+                {/* Book store */}
+                <div
+                    className="space-y-2  rounded-xl"
+                    style={{ boxShadow: "0px 0px 10px 0px #9b59b6" }}
+                >
+                    <img
+                        className="rounded-t-xl w-full max-h-80"
+                        src={bookStore}
+                        alt=""
+                    />
+                    <div className="p-2">
+                        <h1 className="text-lg text-purple-600 font-bold">
+                            Online Secondhand Bookstore.
+                        </h1>
+                        <p className="text-justify">
+                            The Online Secondhand Book Buying and Selling system
+                            is an online based website. It is a platform where
+                            any user can buy or sell secondhand books by staying
+                            in their own place.
+                        </p>
+                    </div>
+                </div>
+
+                {/* Counseling And Mental Health */}
+                <div
+                    className="space-y-2  rounded-xl"
+                    style={{ boxShadow: "0px 0px 10px 0px #9b59b6" }}
+                >
+                    <img
+                        className="rounded-t-xl w-full max-h-80"
+                        src={counseling}
+                        alt=""
+                    />
+                    <div className="p-2">
+                        <h1 className="text-lg text-purple-600 font-bold">
+                            Counseling and Consultancy of Mental Health
+                        </h1>
+                        <p className="text-justify">
+                            Globally, mental health problems are growing a
+                            public health concern. Resources and services for
+                            mental disorders are disproportionately low compared
+                            to physical disorder.In this project we will try to
+                            create such an interface which will help it’s users
+                            to support mentally and resolve their mental problem
+                            through experienced councilors.
+                        </p>
+                    </div>
+                </div>
+
+                {/* Counter App */}
+                <div
+                    className="space-y-2  rounded-xl"
+                    style={{ boxShadow: "0px 0px 10px 0px #9b59b6" }}
+                >
+                    <img
+                        className="rounded-t-xl w-full max-h-80"
+                        src={counter}
+                        alt=""
+                    />
+                    <div className="p-2">
+                        <h1 className="text-lg text-purple-600 font-bold">
+                            Coounter App
+                        </h1>
+                        <p className="text-justify">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Totam magni suscipit quas perferendis, eum
+                            consequuntur laboriosam incidunt voluptatum ratione
+                            magnam. Enim ab eaque illum ad quis recusandae velit
+                            et quod, expedita ratione distinctio error aliquid
+                            provident odit explicabo! A, neque!
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </section>
     );
 };
 
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,27 +1,76 @@
 import React from "react";
 
+import planning from"./../../assets/icons/planning.png"
+import management from"./../../assets/icons/management.png"
+import risk from"./../../assets/icons/risk.png"
+import thinking from"./../../assets/icons/thinking.png"
+import web from"./../../assets/icons/web-development.png"
+import data from"./../../assets/icons/data-science.png"
+
 const Skills = () => {
     return (
-        <section className="max-w-screen-xl flex flex-col items-center gap-8 md:gap-14 lg:gap-20 justify-center mx-auto px-2 py-8 pb-12 min-h-screen text-white ">
-            <h1
-                className="font-bold downMove mb-4 text-center text-transparent lg:text-5xl md:text-4xl text-3xl bg-clip-text bg-gradient-to-r from-purple-600 to-orange-500"
-            >
+        <section className="max-w-screen-xl flex flex-col items-center gap-6 md:gap-10  justify-center mx-auto px-2 py-8 pb-12 min-h-screen text-white ">
+            <h1 className="font-bold  mb-4 text-center text-transparent lg:text-5xl md:text-4xl text-3xl bg-clip-text bg-gradient-to-r from-purple-600 to-orange-500">
                 <span className="text-white">My</span> Skills
             </h1>
 
-            <section
-                className="flex w-full topMove  flex-col md:flex-row gap-10 md:gap-10 items-center justify-center lg:justify-around"
-            >
-                <div
-                    className="skill relative skill-container  list-none  md:w-1/2 w-full px-3 border-2 border-purple-600 p-4 rounded-lg"
-                >
-                    <div className="upEffect absolute  rounded-lg top-0 left-0 "></div>
+            <section className="flex w-full  flex-col gap-10 md:gap-16 items-center justify-center lg:justify-around">
+                
 
-                    <div className="relative">
-                        <h1 className="text-xl md:text-3xl font-bold">
-                            Language and Tools:
-                        </h1>
+                <div className="skill  relative skill-container  list-none   w-full px-3 p-2 rounded-lg">
+                    <h1 className="text-xl text-center md:text-3xl font-bold mb-4 md:mb-6">
+                        Technical Skills:
+                    </h1>
 
+                    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-10">
+                        
+                        {/* Project Planning */}
+                        <div className="bg-[#113f5d] p-4 border-2 border-purple-400  space-y-1 md:space-y-2 rounded-lg text-center">
+                            <img className="w-20 mx-auto" src={planning} alt="" />
+                            <h1 className="text-lg text-Primary md:text-xl font-semibold">Project Planning</h1>
+                            <p className="">Lorem ipsum dolor sit amet.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, possimus.</p>
+                        </div>
+                        {/* Project Management */}
+                        <div className="bg-[#113f5d] p-4 border-2 border-purple-400   space-y-1 md:space-y-2 rounded-lg text-center">
+                            <img className="w-20 mx-auto" src={management} alt="" />
+                            <h1 className="text-lg text-Primary md:text-xl font-semibold">Project Management</h1>
+                            <p className="">Lorem ipsum dolor sit amet.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, possimus.</p>
+                        </div>
+                        {/* rsik Assesment*/}
+                        <div className="bg-[#113f5d] p-4 border-2 border-purple-400   space-y-1 md:space-y-2 rounded-lg text-center">
+                            <img className="w-20 mx-auto" src={risk} alt="" />
+                            <h1 className="text-lg text-Primary md:text-xl font-semibold">Risk Assesment</h1>
+                            <p className="">Lorem ipsum dolor sit amet.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, possimus.</p>
+                        </div>
+                        {/* Critical Thinking */}
+                        <div className="bg-[#113f5d] p-4 border-2 border-purple-400   space-y-1 md:space-y-2 rounded-lg text-center">
+                            <img className="w-20 mx-auto" src={thinking} alt="" />
+                            <h1 className="text-lg text-Primary md:text-xl font-semibold">Critical Thinking</h1>
+                            <p className="">Lorem ipsum dolor sit amet.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, possimus.</p>
+                        </div>
+                        {/* Web Development */}
+                        <div className="bg-[#113f5d] p-4 border-2 border-purple-400   space-y-1 md:space-y-2 rounded-lg text-center">
+                            <img className="w-20 mx-auto" src={web} alt="" />
+                            <h1 className="text-lg text-Primary md:text-xl font-semibold">Web Development</h1>
+                            <p className="">Lorem ipsum dolor sit amet.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, possimus.</p>
+                        </div>
+                        {/* Data Science */}
+                        <div className="bg-[#113f5d] p-4 border-2 border-purple-400   space-y-1 md:space-y-2 rounded-lg text-center">
+                            <img className="w-20 mx-auto" src={data} alt="" />
+                            <h1 className="text-lg text-Primary md:text-xl font-semibold">Data Science</h1>
+                            <p className="">Lorem ipsum dolor sit amet.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, possimus.</p>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div className="skill relative skill-container   list-none  md:w-4/5 w-full px-3   ">
+                    <h1 className="text-xl md:text-3xl text-center font-bold">
+                        Language and Tools:
+                    </h1>
+
+                    <div className="relative border-2 mt-4 p-4 rounded-lg border-purple-600">
                         <li>
                             <div className="flex justify-between px-2 mb-2">
                                 <h3>Javascript</h3>
@@ -74,60 +123,6 @@ const Skills = () => {
                             </div>
                             <span className="bar">
                                 <span className="bg-purple-400 w-[85%]"></span>
-                            </span>
-                        </li>
-                    </div>
-                </div>
-
-                {/* left side */}
-
-                <div
-                    className="skill  relative skill-container  list-none  md:w-1/2 w-full px-3 border-2 border-[#FF6AC2] p-4 rounded-lg"
-                >
-                    <div className="upEffect absolute  rounded-lg top-0 left-0 "></div>
-                    <div className="relative">
-                        <h1 className="text-xl md:text-3xl font-bold">
-                            Technical skills:
-                        </h1>
-
-                        <li>
-                            <div className="flex justify-between px-2 mb-2">
-                                <h3>React</h3>
-                                <h3>70%</h3>
-                            </div>
-                            <span className="bar">
-                                {" "}
-                                <span className="react"></span>
-                            </span>
-                        </li>
-                        <li>
-                            <div className="flex justify-between px-2 mb-2">
-                                <h3>MongoDB</h3>
-                                <h3>60%</h3>
-                            </div>
-                            <span className="bar">
-                                {" "}
-                                <span className="mongodb"></span>
-                            </span>
-                        </li>
-                        <li>
-                            <div className="flex justify-between px-2 mb-2">
-                                <h3>Node JS</h3>
-                                <h3>40%</h3>
-                            </div>
-                            <span className="bar">
-                                {" "}
-                                <span className="node"></span>
-                            </span>
-                        </li>
-                        <li>
-                            <div className="flex justify-between px-2 mb-2">
-                                <h3>Express js</h3>
-                                <h3>25%</h3>
-                            </div>
-                            <span className="bar">
-                                {" "}
-                                <span className="express"></span>
                             </span>
                         </li>
                     </div>
