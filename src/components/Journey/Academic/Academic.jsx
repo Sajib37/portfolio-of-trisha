c7b72b62930d1d35299f0595f60c8ab8cbf18c89
sajib37 <sajibhasan9811@gmail.com>
2024-05-12 09:42:19 +0600

    experience section completed

--- src/components/Journey/Academic/Academic.jsx
+++ src/components/Journey/Academic/Academic.jsx
@@ -1,37 +1,64 @@
-import React from 'react';
+import React from "react";
 import { FaCalendar } from "react-icons/fa";
+import "./../../../App.css"
 
 const Academic = () => {
     return (
         <div>
-             <h1 className="text-xl md:text-3xl mb-4 font-bold">Academic Journey :</h1>
-            <ol className="relative border-s mx-auto border-gray-200 dark:border-gray-700 max-w-xs md:max-w-2xl">                  
+            <h1 className="text-xl md:text-3xl mb-4 font-bold">
+                Academic Journey :
+            </h1>
+            <ol className="relative border-s mx-auto border-gray-200 dark:border-gray-700 max-w-xs md:max-w-2xl">
                 <li className="mb-10 relative container ms-4">
                     <div className="absolute w-3 h-3 bg-purple-600 rounded-full top-1 -left-[23px] border border-purple-600 "></div>
-                    <div className='leftEffect'></div>
+                    <div className="leftEffect"></div>
                     <div className="relative rounded-lg w-full p-1 md:p-3 timeline-box bg-transparent border-2 border-purple-600">
-                        <span className="flex gap-2 items-center text-base"><FaCalendar /> 2019 - 2024</span>
-                        <h1 className="text-lg md:text-xl font-semibold mb-1 md:mb-2">Jagannath University (BSc)</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus qui eligendi non minima voluptas consequuntur inventore laboriosam deleniti aut! Suscipit incidunt quisquam ea ipsa nobis reprehenderit maxime, perferendis assumenda voluptas.</p>
-                    </div>   
+                        <span className="flex gap-2 items-center text-base">
+                            <FaCalendar /> 2019 - 2024
+                        </span>
+                        <h1 className="text-lg md:text-xl font-semibold mb-1 md:mb-2">
+                            Jagannath University (BSc)
+                        </h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Culpa quod excepturi corporis dolorem rem non.
+                            Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
                 </li>
                 <li className="mb-10 ms-4 relative container">
                     <div className="absolute w-3 h-3 bg-purple-600 rounded-full top-1 -left-[23px] border border-purple-600 "></div>
-                    <div className='leftEffect'></div>
+                    <div className="leftEffect"></div>
                     <div className="relative rounded-lg w-full p-1 md:p-3 timeline-box bg-transparent border-2 border-purple-600">
-                        <span className="flex gap-2 items-center text-base"><FaCalendar /> 2016 - 2017</span>
-                        <h1 className="text-lg md:text-xl font-semibold mb-1 md:mb-2">Noakhali  (HSC)</h1>
-                        <p>I completed my Higher Secondary Certificate (HSC) in the science group at Chuadanga Government College, gaining in-depth knowledge and essential skills for future academic and professional challenges.</p>
-                    </div>   
+                        <span className="flex gap-2 items-center text-base">
+                            <FaCalendar /> 2016 - 2017
+                        </span>
+                        <h1 className="text-lg md:text-xl font-semibold mb-1 md:mb-2">
+                            Noakhali (HSC)
+                        </h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing
+                            elit. Architecto adipisci a sequi quis eaque
+                            consequatur eius itaque sunt saepe consequuntur!
+                        </p>
+                    </div>
                 </li>
                 <li className="mb-10 ms-4 relative container">
                     <div className="absolute w-3 h-3 bg-purple-600 rounded-full top-1 -left-[23px] border border-purple-600 "></div>
-                    <div className='leftEffect'></div>
+                    <div className="leftEffect"></div>
                     <div className="relative w-full rounded-lg p-1 md:p-3 timeline-box bg-transparent border-2 border-purple-600">
-                        <span className="flex gap-2 items-center text-base"><FaCalendar /> 2015 - 2016</span>
-                        <h1 className="text-lg md:text-xl font-semibold mb-1 md:mb-2">Gobindahuda High School (SSC)</h1>
-                        <p>I obtained my Secondary School Certificate from Gobindahuda High School in the science group. Immersed in a well-rounded curriculum, I gained essential skills and knowledge for future academic and professional pursuits.</p>
-                    </div>   
+                        <span className="flex gap-2 items-center text-base">
+                            <FaCalendar /> 2014 - 2015
+                        </span>
+                        <h1 className="text-lg md:text-xl font-semibold mb-1 md:mb-2">
+                            Thaker Hat Ahmeed Ullah High School (SSC)
+                        </h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Sit consequuntur cumque aliquam laudantium
+                            dolorum harum quis ut. Fugit, molestias alias?
+                        </p>
+                    </div>
                 </li>
             </ol>
         </div>
--- src/components/Journey/Experience/Experience.jsx
+++ src/components/Journey/Experience/Experience.jsx
@@ -1,27 +1,84 @@
-import React from 'react';
-import { FaCalendar } from 'react-icons/fa';
-
+import React from "react";
+import { FaCalendar } from "react-icons/fa";
+import "./../../../App.css"
 const Experience = () => {
     return (
         <section>
             <h1 className="text-xl md:text-3xl mb-4 font-bold">Experience :</h1>
-            <ol className="relative border-s border-gray-200 dark:border-gray-700 mx-auto max-w-xs md:max-w-2xl">                  
-                <li data-aos="fade-up" data-aos-duration="2000" className="mb-10 container relative ms-4">
-                    <div className="absolute w-3 h-3 bg-[#FF6AC2] rounded-full top-1 -left-[23px] border border-[#FF6AC2] "></div>
-                    <div className='leftEffect'></div>
+            <ol className="relative border-s border-gray-200 dark:border-gray-700 mx-auto max-w-xs md:max-w-2xl">
+                <li className="mb-10 container relative ms-4">
+                    <div className="absolute w-3 h-3 bg-purple-600 rounded-full top-1 -left-[23px] border border-purple-600 "></div>
+                    <div className="leftEffect"></div>
 
-                    <div className="relative w-full py-1 md:py-2 timeline-box bg-transparent border-2 border-[#FF6AC2]">
-                        <h1 className="text-lg md:text-xl font-semibold mb-2 ">Exploring the MERN Stack:</h1>
-                        <p>Exploring the MERN stack, I'm actively learning MongoDB, Express.js, React.js, and Node.js. Committed to gaining hands-on experience in crafting dynamic and responsive web applications.</p>
-                    </div>   
+                    <div className="relative rounded-lg w-full p-1 md:p-3 timeline-box bg-transparent border-2 border-purple-600">
+                        <h1 className="text-lg md:text-xl font-semibold mb-2 ">
+                            Women In Digital-
+                            <span className="text-Primary">intern</span>
+                        </h1>
+                        <p>
+                            Here I have started learning on
+                            Blockchain.Blockchain is a revolutionary technology
+                            that has gained significant attention and popularity
+                            in recent years. I also work on a thesis titled,
+                            “Attribute based access control using hyperledger
+                            blockchain”.
+                        </p>
+                    </div>
                 </li>
-                <li data-aos="fade-up" data-aos-duration="2000" className="mb-10 ms-4 relative container">
-                    <div className="absolute w-3 h-3 bg-[#FF6AC2] rounded-full top-1 -left-[23px] border border-[#FF6AC2] "></div>
-                    <div className='leftEffect'></div>
-                    <div className=" w-full relative py-1 md:py-2 timeline-box bg-transparent border-2 border-[#FF6AC2]">
-                        <h1 className="text-lg md:text-xl font-semibold mb-2">Foundations in C , C++ and Java:</h1>
-                        <p>With a foundation in C, C++, and Java, I'm building the groundwork for my programming journey. Though not highly experienced, I contribute to coding tasks and grasp core principles of software development.</p>
-                    </div>   
+
+                <li className="mb-10 container relative ms-4">
+                    <div className="absolute w-3 h-3 bg-purple-600 rounded-full top-1 -left-[23px] border border-purple-600 "></div>
+                    <div className="leftEffect"></div>
+
+                    <div className="relative rounded-lg w-full p-1 md:p-3 timeline-box bg-transparent border-2 border-purple-600">
+                        <h1 className="text-lg md:text-xl font-semibold mb-2 ">
+                            Women In Digital-
+                            <span className="text-Primary">
+                                Blockchain Trainer
+                            </span>
+                        </h1>
+                        <p>
+                            Here iam working as a Blockchain Trainer and took
+                            several nationwide codingcamp in many universities
+                            of Bangladesh.
+                        </p>
+                    </div>
+                </li>
+
+                <li className="mb-10 container relative ms-4">
+                    <div className="absolute w-3 h-3 bg-purple-600 rounded-full top-1 -left-[23px] border border-purple-600 "></div>
+                    <div className="leftEffect"></div>
+
+                    <div className="relative rounded-lg w-full p-1 md:p-3 timeline-box bg-transparent border-2 border-purple-600">
+                        <h1 className="text-lg md:text-xl font-semibold mb-2 ">
+                            Farhana’s Brainstation-
+                            <span className="text-Primary">
+                                Project Associate(Volunteer)
+                            </span>
+                        </h1>
+                        <p>
+                            Here Iam working as an IT specialist.And doing a
+                            project under SDG for ensuring quality education..
+                        </p>
+                    </div>
+                </li>
+                <li className="mb-10 container relative ms-4">
+                    <div className="absolute w-3 h-3 bg-purple-600 rounded-full top-1 -left-[23px] border border-purple-600 "></div>
+                    <div className="leftEffect"></div>
+
+                    <div className="relative rounded-lg w-full p-1 md:p-3 timeline-box bg-transparent border-2 border-purple-600">
+                        <h1 className="text-lg md:text-xl font-semibold mb-2 ">
+                            SimpliSolve-
+                            <span className="text-Primary">
+                                Data Science(Trainee)
+                            </span>
+                        </h1>
+                        <p>
+                            Here iam working remotely in Data Science track.And
+                            learning different types of ML models and works with
+                            different datasets..
+                        </p>
+                    </div>
                 </li>
             </ol>
         </section>
